d03d7120f8a1baa634c0b416fd980a8d7963cb69
ryouheishimizuhata <ameshikou2828@gmail.com>
2023-07-21 14:16:47 +0900

    show all table pegination

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])
 
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-
-
-Route::get('/posts', [PostController::class, 'index']);   
+?>
