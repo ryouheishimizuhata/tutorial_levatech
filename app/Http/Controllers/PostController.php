9c20bd3944c5970c46dd3204b2135df808f86843
ryouheishimizuhata <ameshikou2828@gmail.com>
2023-07-30 19:30:25 +0900

    投稿機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 /**
  * Post一覧の表示
@@ -16,11 +17,23 @@ class PostController extends Controller
 {
     public function index(Post $post) //インポートしたPostをインスタンス化して使用する
     {
-        return view('posts/index')->with(['posts' => $post->get()]);
+       return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
     
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 */
 
 
-Route::get('/', [PostController::class, 'index']);   
+Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
